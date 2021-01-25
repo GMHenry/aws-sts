214f80e00dcd08cae2f60aabf49663680d495761
Enrique <enrique@ubuntu1804.linuxvmimages.local>
2021-01-25 12:49:56 -0600

    ready

---
+++ filter_plugins/dotted_dict.py
@@ -0,0 +1,23 @@
+import operator
+import json
+from collections import defaultdict
+
+class FilterModule(object):
+  ''' Converts CloudFormation Template Parameters to Stack Input mappings '''
+  def filters(self):
+    return {
+        'dotted_dict': dotted_dict
+    }
+
+def dotted_dict(vars, paths=[]):
+  infinitedict = lambda: defaultdict(infinitedict)
+  data = infinitedict()
+  vars_keys = vars.keys()
+  vars_keys = sorted(key=len)
+  params = [(k,vars[k]) for p in paths for k in vars_keys if k.startswith(p) ]
+  for param in params:
+    keys = param[0].split('.')
+    print("-",keys)
+    parent = reduce(operator.getitem, keys[:-1], data)
+    parent[keys[-1]] = param[1]
+  return json.loads(json.dumps(data))
